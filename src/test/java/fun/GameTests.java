683d7ba5ecb6273f5ec84e8a0699c57fddd89c40
yabekt <ybtibebu@gmail.com>
2020-06-25 15:07:40 -0400

    Moved runner to GameTests.

--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,16 @@
 package fun;
 
-public class GameTests {
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/java/fun", glue = {
+							"com.fun.steps" }, 
+							dryRun = true, 
+							tags = "@DefaultGameTest, @NamedGameTest")
+public class GameTests {
 
 }
