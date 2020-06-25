efc2355ae7599203f93a07e2adb8c62f6fbc30de
Mary <marykavitha1993@gmail.com>
2020-06-24 20:09:36 -0400

    maryscode

--- src/main/java/fun/Game.java
+++ src/main/java/fun/Game.java
@@ -2,8 +2,11 @@ package fun;
 
 import lombok.Builder;
 import lombok.Getter;
+//import static TypeThatHasABuilder.*;
 
-@Getter @Builder
+
+@Getter
+@Builder
 public class Game {
     private Integer id;
     private String text;
--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,59 @@
 package fun;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
 public class GameTests {
-
-
+	
+	@Test
+	//checking for chess
+	public void getgame() {
+		RestAssured.baseURI= "http://localhost:8080/";
+		String inputgame="Chess";
+		String response=given().log().all().queryParam("name", inputgame).header("Content-Type","application/json")
+				.when().get("/game").then().assertThat().log().all().statusCode(200).extract().response().asString();
+		System.out.println(response);
+		JsonPath jp=new JsonPath(response);
+		String gamename=jp.getString("text");
+		System.out.println(gamename);
+		assertTrue(gamename.contains(inputgame));
+	}
+	
+	
+	@Test
+	//checking for numeric name
+	public void getgamenumeric() {
+		RestAssured.baseURI= "http://localhost:8080/";
+		String inputgame="1235";
+		String response=given().log().all().queryParam("name", inputgame).header("Content-Type","application/json")
+				.when().get("/game").then().assertThat().log().all().statusCode(200).extract().response().asString();
+		System.out.println(response);
+		JsonPath jp=new JsonPath(response);
+		String gamename=jp.getString("text");
+		System.out.println(gamename);
+		assertTrue(gamename.contains(inputgame));
+	}
+	@Test
+	//checking for sudoku
+	public void getgamesudoku() {
+		RestAssured.baseURI= "http://localhost:8080/";
+		String response=given().log().all().header("Content-Type","application/json")
+				.when().get("/game").then().assertThat().log().all().statusCode(200).extract().response().asString();
+		System.out.println(response);
+		JsonPath jp=new JsonPath(response);
+		String gamename=jp.getString("text");
+		System.out.println(gamename);
+		assertTrue(gamename.contains("Sudoku"));
+	}
+	
 }
+
+
+
