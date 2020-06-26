dc528174b615d122b3a9f623b9f26079e48a0fa5
YidnekachewEndale <yidnekachew.endale@gmail.com>
2020-06-26 16:38:43 -0400

    Functional Test For Game Identifier. Yidnekachew Endale

---
+++ src/test/java/fun/FunctionalTest.java
@@ -0,0 +1,31 @@
+package fun;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class FunctionalTest {
+    @BeforeClass
+    public static void setup() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8080);
+        }
+        else{
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+
+        String basePath = System.getProperty("server.base");
+        if(basePath==null){
+            basePath = "/game/";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseHost = System.getProperty("server.host");
+        if(baseHost==null){
+            baseHost = "http://localhost";
+        }
+        RestAssured.baseURI = baseHost;
+
+    }
+}
--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,95 @@
 package fun;
 
-public class GameTests {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
+public class GameTests extends FunctionalTest{
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    //Verify the service is up and running
+    public void pingTest(){
+        Response response =  given().when().get();
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
+    }
+
+    @Test
+    //Verify if the service returns the default game
+    public void defaultGameTest(){
+        String defaultResponse =  given().when().get().then().extract().asString();
+        String defaultText = null;
+        try {
+            JsonNode dfltRsp = mapper.readTree(defaultResponse);
+            defaultText = dfltRsp.get("text").asText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(defaultResponse != null);
+        Assert.assertEquals(defaultText,"Playing Sudoku is fun!");
+    }
+
+    @Test
+    //Verify that input game is returned back
+    public void inputGameTest(){
+        String inputGameResponse =  given().queryParam("name", "Chess").when().get().then().extract().asString();
+        String inputGameText = null;
+        try {
+            JsonNode dfltRsp = mapper.readTree(inputGameResponse);
+            inputGameText = dfltRsp.get("text").asText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(inputGameResponse != null);
+        Assert.assertEquals(inputGameText,"Playing Chess is fun!");
+    }
+
+    @Test
+    //Verify If game Id isn't 0
+    public void inputGameIdTest(){
+        String inputGameResponse =  given().queryParam("name", "Chess").when().get().then().extract().asString();
+        int inputGameId = 0;
+        try {
+            JsonNode dfltRsp = mapper.readTree(inputGameResponse);
+            inputGameId = dfltRsp.get("id").asInt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(inputGameResponse != null);
+        Assert.assertTrue(inputGameId != 0);
+    }
+
+
+    @Test
+    //Verify the service doesn't return a duplicated Id.
+    public void inputGameDifferentIdTest(){
+        String inputGameResponse =  given().queryParam("name", "Chess").when().get().then().extract().asString();
+        String secondInputGameResponse =  given().queryParam("name", "Soccer").when().get().then().extract().asString();
+        int inputGameId = 0;
+        int secondInputGameId = 0;
+        try {
+            JsonNode dfltRsp = mapper.readTree(inputGameResponse);
+            inputGameId = dfltRsp.get("id").asInt();
+
+            JsonNode dfltRsp2 = mapper.readTree(secondInputGameResponse);
+            secondInputGameId = dfltRsp2.get("id").asInt();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(inputGameId != secondInputGameId);
+    }
 
 }
